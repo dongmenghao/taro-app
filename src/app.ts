ac4a604365e193ef1559ce19e825beaa6391742f
dongmenghao <dongmh105@qq.com>
2023-05-04 15:27:35 +0800

    format code

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDidShow, useDidHide } from '@tarojs/taro'
 // 全局样式
-import './app.less'
 import 'windi.css'
+import './app.less'
 
 function App(props) {
   // 可以使用所有的 React Hooks
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { View } from '@tarojs/components'
-import {
-  Button
-} from "@nutui/nutui-react-taro";
+import { Button } from '@nutui/nutui-react-taro'
 import './index.less'
 
 function Index() {
   return (
-    <View className="nutui-react-demo">
-      <View className="index">
-        欢迎使用 NutUI React 开发 Taro 多端项目。
-      </View>
-      <View className="index">
-        <Button type="primary" className="btn !bg-yellow-500">
+    <View className='nutui-react-demo'>
+      <View className='index'>欢迎使用 NutUI React 开发 Taro 多端项目。</View>
+      <View className='index'>
+        <Button type='primary' className='btn !bg-yellow-500'>
           NutUI React Button
         </Button>
         <View className=' text-red text-xs'>Test</View>
