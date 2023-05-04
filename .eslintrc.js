c0c31bbbf8298a06613228dca9fee9475db79dfa
dongmenghao <dongmh105@qq.com>
2023-05-04 14:59:13 +0800

    chore: config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['taro'],
+  rules: {
+    // 在这里添加你的自定义规则
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     singleQuote: true,
+    //     trailingComma: 'all',
+    //   },
+    // ],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
