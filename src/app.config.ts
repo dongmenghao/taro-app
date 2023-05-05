35625305e467811cece56b8dfdd21658b3c70a76
dongmenghao <dongmh105@qq.com>
2023-05-05 18:28:43 +0800

    feat(tabbar): 添加tabbar, 我的页面

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,28 @@
 export default defineAppConfig({
-  pages: ['pages/index/index'],
+  pages: [
+    'pages/index/index',
+    'pages/my/index'
+  ],
+  tabBar: {
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: 'assets/icons/home.png',
+        selectedIconPath: 'assets/icons/home-select.png',
+      },
+      {
+        pagePath: 'pages/my/index',
+        text: '我的',
+        iconPath: 'assets/icons/my.png',
+        selectedIconPath: 'assets/icons/my-select.png',
+      },
+    ],
+    color: '#000',
+    selectedColor: '#f85f4b',
+    backgroundColor: '#fff',
+    borderStyle: 'white',
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
---
+++ src/pages/my/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from '@tarojs/components'
+import './index.less'
+
+function Index() {
+  return (
+    <View className='nutui-react-demo'>
+      <View className='index'>
+        我的111
+      </View>
+    </View>
+  )
+}
+
+export default Index
